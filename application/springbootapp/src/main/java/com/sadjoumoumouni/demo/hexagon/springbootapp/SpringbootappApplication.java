fd471547cbe920c4731c69056e58c95ec0a3a4a4
Sydney Adjou-Moumouni <Sydney.ADJOU-MOUMOUNI@cpexterne.org>
2020-10-19 23:16:54 +0200

    remove targets

--- application/springbootapp/src/main/java/com/sadjoumoumouni/demo/hexagon/springbootapp/SpringbootappApplication.java
+++ application/springbootapp/src/main/java/com/sadjoumoumouni/demo/hexagon/springbootapp/SpringbootappApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.sadjoumoumouni.demo.hexagon.adapters.product.drivens")
 @EntityScan(basePackages = "com.sadjoumoumouni.demo.hexagon.domain")
